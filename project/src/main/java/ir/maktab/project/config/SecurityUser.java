70163570d0133c5c422d56a683909ed8f3b8b98e
mohammad nasr <nasrmohammad4804@gmail.com>
2021-11-12 10:44:22 +0330

    this file SecurityUser is Continues of spring security

---
+++ project/src/main/java/ir/maktab/project/config/SecurityUser.java
@@ -0,0 +1,52 @@
+package ir.maktab.project.config;
+
+import ir.maktab.project.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class SecurityUser implements UserDetails {
+
+    private static final String  ROLE_PREFIX="ROLE_";
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(ROLE_PREFIX.concat(user.getRole().getName())));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getIsActive();
+    }
+}
