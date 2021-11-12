4f5d6960c4ee6584c8908325aa5979f507ee502d
mohammad nasr <nasrmohammad4804@gmail.com>
2021-11-12 10:38:32 +0330

    add exam repository and change CourseRepository

--- project/src/main/java/ir/maktab/project/repository/CourseRepository.java
+++ project/src/main/java/ir/maktab/project/repository/CourseRepository.java
@@ -1,8 +1,13 @@
 package ir.maktab.project.repository;
 
 import ir.maktab.project.domain.Course;
+import ir.maktab.project.domain.Master;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 import java.util.Optional;
 
@@ -13,4 +18,10 @@ public interface CourseRepository extends JpaRepository<Course,Long> {
 
      @EntityGraph(attributePaths = "students")
      Optional<Course> findById(Long id);
+
+     @EntityGraph(attributePaths = "examList")
+     @Query("select c from Course as c where c.id=:id")
+     Optional<Course> findWithId(@Param("id") Long id);
+
+     List<Course> findByMaster(Master master);
 }
---
+++ project/src/main/java/ir/maktab/project/repository/ExamRepository.java
@@ -0,0 +1,9 @@
+package ir.maktab.project.repository;
+
+import ir.maktab.project.domain.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExamRepository extends JpaRepository<Exam,Long> {
+}
