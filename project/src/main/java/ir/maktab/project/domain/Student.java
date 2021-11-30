2e9c2a0e29819be7e6c738497e2c1ddc0d59b64e
mohammad nasr <nasrmohammad4804@gmail.com>
2021-11-30 10:28:55 +0330

    add two field to student contains(answerList,studentExamStates) for student can be have many examState and many answer on different answer in exams

--- project/src/main/java/ir/maktab/project/domain/Student.java
+++ project/src/main/java/ir/maktab/project/domain/Student.java
@@ -1,12 +1,13 @@
 package ir.maktab.project.domain;
 
+import ir.maktab.project.domain.embeddable.StudentExamState;
 import ir.maktab.project.domain.enumeration.RegisterState;
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -17,7 +18,9 @@ import java.util.Set;
 public class Student extends User {
 
     private static final String STUDENT_CODE = "student_code";
-    private static final String FATHER_NAME = "father_name" ;
+    private static final String FATHER_NAME = "father_name";
+    public static final String STUDENT_ID = "student_id";
+    private static final String STUDENT_EXAM_STATE = "student_exam_state";
 
     @Column(name = FATHER_NAME)
     private String fatherName;
@@ -31,5 +34,12 @@ public class Student extends User {
     }
 
     @ManyToMany(mappedBy = "students")
-    private Set<Course> courseSet=new HashSet<>();
+    private Set<Course> courseSet = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "student")
+    private List<Answer> answerList = new ArrayList<>();
+
+    @ElementCollection
+    @JoinTable(name = STUDENT_EXAM_STATE, joinColumns = @JoinColumn(name = STUDENT_ID))
+    private List<StudentExamState> studentExamStates = new ArrayList<>();
 }
