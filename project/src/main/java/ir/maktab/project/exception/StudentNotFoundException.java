6eb58fc78fe1052aae97fb548385da6b2cacd2b6
mohammad nasr <nasrmohammad4804@gmail.com>
2021-11-30 10:08:13 +0330

    add StudentNotFoundException for when find that by id and dont find get error on client not in server

---
+++ project/src/main/java/ir/maktab/project/exception/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package ir.maktab.project.exception;
+
+public class StudentNotFoundException extends RuntimeException{
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
