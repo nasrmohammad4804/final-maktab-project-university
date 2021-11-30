7036e1c07326894084c25f41f8ddde7a1344cc9f
mohammad nasr <nasrmohammad4804@gmail.com>
2021-11-30 10:02:29 +0330

    add many query for different uses contain get student with all state in different exam or get all answer

--- project/src/main/java/ir/maktab/project/repository/StudentRepository.java
+++ project/src/main/java/ir/maktab/project/repository/StudentRepository.java
@@ -1,5 +1,6 @@
 package ir.maktab.project.repository;
 
+import ir.maktab.project.domain.Answer;
 import ir.maktab.project.domain.Student;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,13 +13,27 @@ import java.util.Optional;
 import java.util.Set;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student,Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
 
-    @Query("select s from Student as s where (:checker='' or s not in (:users)) " )
-     List<Student> dontExistOnCourse(@Param("users") Set<Student> users,String checker);
+    @Query("select s from Student as s where (:checker='' or s not in (:users)) ")
+    List<Student> dontExistOnCourse(@Param("users") Set<Student> users, String checker);
 
     @EntityGraph(attributePaths = "courseSet")
     @Query("select s from Student as s where s.id=:id")
-     Optional<Student> findWithId(Long id);
+    Optional<Student> findWithId(Long id);
+
+    @EntityGraph(attributePaths = "studentExamStates")
+    @Query("select s from Student as s where s.id=:id")
+    Optional<Student> findWithAllStudentExamState(Long id);
+
+    @EntityGraph(attributePaths = "answerList")
+    @Query("select s from Student as s where s.id=:id")
+    Optional<Student> findWithAllAnswerList(Long id);
+
+    @Query("select a from Exam  as e join  e.examQuestionList as eq join eq.answerList as a join a.student as s where s.id=:studentId and e.id=:examId")
+    List<Answer> findAllAnswerOfExam(Long studentId,Long examId);
+
+    @Query("select s from Student as s left join s.studentExamStates as ses where ses.examId=:examId and (ses.examState in ('TRYING','FINISHED') )")
+     List<Student> getAllStudentWitchParticipatedInExam(Long examId);
 }
