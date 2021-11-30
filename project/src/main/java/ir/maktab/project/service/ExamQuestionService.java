f7cc5e70369c58d8489a2da7040ca1552b52fa1e
mohammad nasr <nasrmohammad4804@gmail.com>
2021-11-30 10:05:51 +0330

    add findAllDescriptive & getAllQuestion to ExamQuestionService

--- project/src/main/java/ir/maktab/project/service/ExamQuestionService.java
+++ project/src/main/java/ir/maktab/project/service/ExamQuestionService.java
@@ -5,6 +5,7 @@ import ir.maktab.project.domain.ExamQuestion;
 import ir.maktab.project.domain.Question;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -13,5 +14,8 @@ public interface ExamQuestionService {
     void addExamQuestionFromQuestionBank(Exam exam, Question question);
     void addNewDescriptiveQuestion(Exam exam, ExamQuestion examQuestion);
     void addNewTestQuestion(Exam exam, Map<String,?> map);
+    ExamQuestion findById(Long id);
+    List<ExamQuestion> getAllQuestion(Long examId);
 
+    List<ExamQuestion> findAllDescriptiveQuestion(Long examId);
 }
