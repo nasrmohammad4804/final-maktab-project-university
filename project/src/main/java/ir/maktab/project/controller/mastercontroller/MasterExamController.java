2d41f27f920e3cf2e45f56d61974f72979edf0c4
mohammad nasr <nasrmohammad4804@gmail.com>
2021-11-30 10:16:30 +0330

    update MasterExamController and add toScore for master can be give score to answer of student

--- project/src/main/java/ir/maktab/project/controller/mastercontroller/MasterExamController.java
+++ project/src/main/java/ir/maktab/project/controller/mastercontroller/MasterExamController.java
@@ -3,11 +3,13 @@ package ir.maktab.project.controller.mastercontroller;
 import ir.maktab.project.domain.Course;
 import ir.maktab.project.domain.Exam;
 import ir.maktab.project.domain.Master;
+import ir.maktab.project.domain.dto.DescriptiveAnswerDTO;
+import ir.maktab.project.domain.dto.ExamQuestionsWithStudentDescriptiveAnswerDTO;
 import ir.maktab.project.mapper.CourseMapper;
 import ir.maktab.project.mapper.ExamMapper;
-import ir.maktab.project.mapper.UserMapper;
-import ir.maktab.project.service.CourseService;
-import ir.maktab.project.service.ExamService;
+import ir.maktab.project.mapper.ExamQuestionsWithStudentDescriptiveAnswersMapper;
+import ir.maktab.project.mapper.ExamWithStudentMapper;
+import ir.maktab.project.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +26,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/master")
@@ -36,10 +39,26 @@ public class MasterExamController {
     private CourseMapper courseMapper;
 
     @Autowired
+    @Qualifier("examMapperImpl")
     private ExamMapper examMapper;
 
     @Autowired
+    private ExamWithStudentMapper examWithStudentMapper;
+
+    @Autowired
     private ExamService examService;
+
+    @Autowired
+    private StudentService studentService;
+
+    @Autowired
+    private MasterService masterService;
+
+    @Autowired
+    private ExamQuestionsWithStudentDescriptiveAnswersMapper questionsWithStudentDescriptiveAnswersMapper;
+
+    @Autowired
+    private ExamQuestionService examQuestionService;
 
     @GetMapping(value = "/panel")
     public String getMasterPanel(HttpServletRequest request, Model model, HttpServletResponse response) {
@@ -59,10 +78,24 @@ public class MasterExamController {
 
         return "master/showAllExam";
     }
+
+    /*
+   if master before exam want to see exam he see information of exam but after time of exam see both information
+   and list of student participated
+   * */
     @GetMapping(value = "/exam/{id}")
-    public String showExam(@PathVariable("id") Long id,Model model){
-        model.addAttribute("exam",  examMapper.convertEntityToExamDTO(examService.findById(id)));
-        return "master/showExam";
+    public String showExam(@PathVariable("id") Long id, Model model) {
+        Exam exam = examService.findById(id);
+
+        if (!LocalDateTime.now().isAfter(exam.getStartTime().toLocalDate().atTime(exam.getEndTime()))) {
+            model.addAttribute("exam", examMapper.convertEntityToExamDTO(exam));
+            return "master/showExam";
+        }
+
+        model.addAttribute("examWithStudents", examWithStudentMapper.convertExamWithStudentsToExamWithStudentDTO(
+                exam, studentService.findAllStudentWitchParticipatedInExam(id)
+        ));
+        return "master/allStudentParticipated";
     }
 
     @GetMapping(value = "/create-exam/{id}")
@@ -103,7 +136,7 @@ public class MasterExamController {
     @PostMapping(value = "/change-examBeingHeld")
     public String changeExamIsBeingHeld(Exam updatedExam, RedirectAttributes attributes) {
 
-       Exam exam= examService.findById(updatedExam.getId());
+        Exam exam = examService.findById(updatedExam.getId());
         if (!updatedExam.getEndTime().isAfter(LocalTime.now())) {
             attributes.addAttribute("error", "your newTime is not valid");
             return "redirect:/master/change-examBeingHeld?examId=" + updatedExam.getId();
@@ -124,9 +157,9 @@ public class MasterExamController {
     }
 
     @GetMapping(value = "/change-examNotStarted")
-    public String changeExamNotStarted(@RequestParam("id") Long examId, Model model,HttpServletResponse response) {
-        Cookie cookie=new Cookie("examId",String.valueOf(examId));
-        cookie.setMaxAge(60*60*12);
+    public String changeExamNotStarted(@RequestParam("id") Long examId, Model model, HttpServletResponse response) {
+        Cookie cookie = new Cookie("examId", String.valueOf(examId));
+        cookie.setMaxAge(60 * 60 * 12);
         response.addCookie(cookie);
         model.addAttribute("examId", examId);
         return "master/changeExamNotStarted";
@@ -135,7 +168,7 @@ public class MasterExamController {
     @GetMapping(value = "/update-examNotStarted/{examId}")
     public String changeExamNotStarted(Model model, @PathVariable("examId") Long id, @ModelAttribute("error") String error) {
 
-        Exam exam=examService.findById(id);
+        Exam exam = examService.findById(id);
         model.addAttribute("exam", exam);
 
         return "master/updateExamNotStarted";
@@ -144,7 +177,7 @@ public class MasterExamController {
     @PostMapping(value = "/update-examNotStarted")
     public String changeExamNotStarted(Exam updatedExam, RedirectAttributes attributes) {
 
-       Exam exam= examService.findById(updatedExam.getId());
+        Exam exam = examService.findById(updatedExam.getId());
 
         LocalDateTime currentTime = LocalDateTime.now();
         Predicate<LocalDateTime> predicate = (currentTime::isAfter);
@@ -158,4 +191,29 @@ public class MasterExamController {
         examService.updateExamNotStarted(exam, updatedExam);
         return "master/resultOfChangeExamNotStarted";
     }
+
+    @GetMapping("/to-score")
+    public ResponseEntity<ExamQuestionsWithStudentDescriptiveAnswerDTO> setScoreForStudentExam(@RequestParam("studentId") Long studentId, @RequestParam("examId") Long examId) {
+
+        return ResponseEntity.ok(
+                questionsWithStudentDescriptiveAnswersMapper
+                        .convertEntityToExamQuestionWithDescriptiveAnswer(
+                                examQuestionService.findAllDescriptiveQuestion(examId),
+                                studentService.findAllDescriptiveAnswerOfStudentExam(studentId, examId)
+
+                        )
+        );
+    }
+
+    /*
+     * this api for master can be set score for answer of student participated in exam
+     * */
+    @PutMapping("/to-score/{studentId}/{examId}")
+    public ResponseEntity<String> updateScoreForAnswerOfStudent(@RequestBody List<DescriptiveAnswerDTO> answerDTOList, @PathVariable("studentId") Long studentId, @PathVariable("examId") Long examId) {
+
+        masterService.scoreToAnswer(answerDTOList, studentId, examId);
+
+        return ResponseEntity.ok("successfully student answers were graded.");
+    }
+
 }
