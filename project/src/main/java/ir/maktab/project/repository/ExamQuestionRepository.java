de3d809011f3842ef439107541f6d2562d2e9147
mohammad nasr <nasrmohammad4804@gmail.com>
2021-11-30 10:05:11 +0330

    add getAllExamQuestion for show to student in online exam

--- project/src/main/java/ir/maktab/project/repository/ExamQuestionRepository.java
+++ project/src/main/java/ir/maktab/project/repository/ExamQuestionRepository.java
@@ -1,7 +1,18 @@
 package ir.maktab.project.repository;
 
 import ir.maktab.project.domain.ExamQuestion;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ExamQuestionRepository extends JpaRepository<ExamQuestion,Long> {
+import java.util.List;
+
+public interface ExamQuestionRepository extends JpaRepository<ExamQuestion, Long> {
+
+    @EntityGraph(attributePaths = "answerList")
+    @Query("select eq from ExamQuestion as eq join eq.answerList as a where a.student is null and eq.exam.id=:examId")
+    List<ExamQuestion> getAllExamQuestion(@Param("examId") Long examId);
+
+
 }
