fcdb70f483cbd0f0ae0f5b16951495c17f436c59
mohammad nasr <nasrmohammad4804@gmail.com>
2021-12-14 18:52:58 +0330

    add ResetPasswordDTO for when user send newPassword and token  already have it

---
+++ project/src/main/java/ir/maktab/project/domain/dto/ResetPasswordDTO.java
@@ -0,0 +1,15 @@
+package ir.maktab.project.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResetPasswordDTO {
+    private String password;
+    private String myToken;
+}
