bf9808593aba85f310a039e0fe0906d6708fa632
mohammad nasr <nasrmohammad4804@gmail.com>
2021-12-14 18:45:29 +0330

    add getUsernamePasswordAuthenticationFilter for specify if filter failed then go to /login?error=true or authentication is successful go to /login-user

--- project/src/main/java/ir/maktab/project/config/SecurityConfig.java
+++ project/src/main/java/ir/maktab/project/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package ir.maktab.project.config;
 
+import ir.maktab.project.filter.RecaptchaFilter;
+import ir.maktab.project.service.RecaptchaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,18 +10,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private RecaptchaService recaptchaService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.formLogin().defaultSuccessUrl("/login-user").failureUrl("/login?error=true");
+        http.addFilterBefore(getUsernamePasswordAuthenticationFilter(), RecaptchaFilter.class).formLogin().loginPage("/login");
+        http.authorizeRequests().mvcMatchers("/", "", "/login", "/resource/**").permitAll();
         http.authorizeRequests().mvcMatchers("/master/**").hasRole("master")
                 .mvcMatchers("/student/**").hasRole("student")
-        .mvcMatchers("/course/**").hasRole("manager");
+                .mvcMatchers("/course/**").hasRole("manager");
         http.authorizeRequests().mvcMatchers("/WEB-INF/views/accessDenied.jsp").authenticated();
         http.exceptionHandling().accessDeniedPage("/WEB-INF/views/accessDenied.jsp");
         http.cors().and().csrf().disable();
@@ -29,5 +39,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder getPasswordEncoder() {
 
         return new BCryptPasswordEncoder();
+    }
+
+    public UsernamePasswordAuthenticationFilter getUsernamePasswordAuthenticationFilter() throws Exception {
+
+        RecaptchaFilter filter = new RecaptchaFilter(recaptchaService);
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setAuthenticationFailureHandler(new SimpleUrlAuthenticationFailureHandler("/login?error=true"));
+        filter.setAuthenticationSuccessHandler(new SimpleUrlAuthenticationSuccessHandler("/login-user"));
+        return filter;
     }
 }
